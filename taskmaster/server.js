3f63118780ca8e8eb6c2e5d7b53fc4dbd398a20e
Nico Michel <nicomichel@utexas.edu>
2022-01-14 22:45:13 -0600

    Added more to server

--- taskmaster/server.js
+++ taskmaster/server.js
@@ -6,3 +6,9 @@ const app = express();
 var corsOptions = {
     origin: 'http://localhost:8081'
 }
+
+//Express options for reading port options, data (json) and middleware parsing
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
