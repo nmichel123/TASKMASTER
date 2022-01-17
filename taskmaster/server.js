d2b024774cb28bf6cc88e7cafb389f2356516e90
Nico Michel <nicomichel@utexas.edu>
2022-01-16 22:05:21 -0600

    Completed Server file for now and deleted unnecessary files

--- taskmaster/server.js
+++ taskmaster/server.js
@@ -12,3 +12,16 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
+//Sample route
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello TaskMaster!'})
+});
+
+// Add routes requirement here 
+
+//Port
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
+
