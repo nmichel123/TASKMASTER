4dc806f2bf48e508613445d7f7674f992a8d15ea
Nico Michel <nicomichel@utexas.edu>
2022-01-18 23:02:25 -0600

    Created framework for data service

---
+++ taskmaster/src/services/http-common.js
@@ -0,0 +1,33 @@
+import http from '../http-common'
+
+class TaskmasterDataService {
+    getAll() {
+
+    }
+
+    get(id) {
+
+    }
+
+    create(data) {
+
+    }
+
+    update(id,data) {
+
+    }
+
+    delete(id) {
+
+    }
+
+    deleteAll() {
+
+    }
+
+    findByTitle(title) {
+
+    }
+}
+
+export default new TaskmasterDataService
