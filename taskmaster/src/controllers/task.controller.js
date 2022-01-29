cba75e26991dfe7f10acd6079f2540e18ec6bbfa
Nico Michel <nicomichel@utexas.edu>
2022-01-29 12:20:26 -0600

    Initialized task controller

---
+++ taskmaster/src/controllers/task.controller.js
@@ -0,0 +1,38 @@
+const db = require('../models');
+const Task = db.tasks
+const Op = db.Sequelize.Op
+
+// New Task
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all tasks
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single Task with an ID
+exports.findOne = (req, res) => {
+
+};
+
+// Update a Task by the ID in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a Task by the specified ID in the request
+exports.delete = (req, res) => {
+
+};
+
+// Delete all Tasks from the database
+exports.deleteAll = (req, res) => {
+
+};
+
+// Find all published Tasks 
+exports.findAllPublished = (req, res) => {
+
+};
