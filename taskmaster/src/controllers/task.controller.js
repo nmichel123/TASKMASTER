06bc7f4d209eb95534de68bbc444e844f56314b9
Nico Michel <nicomichel@utexas.edu>
2022-01-29 14:43:32 -0600

    Working on controller

--- taskmaster/src/controllers/task.controller.js
+++ taskmaster/src/controllers/task.controller.js
@@ -4,21 +4,97 @@ const Op = db.Sequelize.Op
 
 // New Task
 exports.create = (req, res) => {
+    // Validates requests
+    if (!req.body.title) {
+        res.status(400).send({
+            message: 'Task must be titled!'
+        });
+        return;
+    }
 
+    // Creates a task
+    const task = {
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    }
+
+    // Saves task in the database 
+    Task.create(task)
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+                err.message || 'Error creating task. Try again.'
+        })
+    })
 };
 
 // Retrieve all tasks
 exports.findAll = (req, res) => {
+    const title = req.query.title;
+    var condition = title ? { title: { [Op.like]: `%${title}%`} } : null;
+
+    Task.findAll({ where: condition })
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+                err.message || 'Error retrieving tasks.'
+        })
+    })
 
 };
 
 // Find a single Task with an ID
 exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    Task.findByPk(id)
+    .then(data => {
+        if (data) {
+            res.send(data);
+        } else {
+            res.status(404).send({
+                message: `Cannot find Task # ${id}`
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: 'Error retrieving Task #' + id
+        })
+    })
 
 };
 
 // Update a Task by the ID in the request
 exports.update = (req, res) => {
+    const id = req.params.id;
+
+    Task.update(req.body, {
+        where: { id : id }
+    })
+    .then(num => {
+        if (num === 1) {
+            res.send({
+                message: 'Task updated successfully'
+            });
+        } else {
+            res.send({
+                message: `Cannot update Task # ${id}. Task may be missing or field is empty!`
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: 'Error updating Task # ' + id
+        });
+    });
 
 };
 
